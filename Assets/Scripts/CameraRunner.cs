63a9ebdcc17d261f44deefe976ab118c9a424c87
hiepledinh <hiepledinh@gmail.com>
2014-06-10 01:34:26 +0700

    unity
    hope success

---
+++ Assets/Scripts/CameraRunner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRunner : MonoBehaviour
+{
+
+	public Transform player;
+	void Update ()
+	{
+		if (player.position.y > transform.position.y) {
+			transform.position = new Vector3 (0, player.position.y, -10);
+		}
+	}
+}
